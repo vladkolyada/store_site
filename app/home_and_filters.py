d2ef2b343c6bb2c12a65c495049b7273721f5ca8
vladkolyada <v.kolyada7.13@gmail.com>
2023-05-30 10:55:03 +0300

    треба доробити файл base.html. Кнопки sign up і log in повинні бути тільки якщо користувач не зареєстрований або якщо він не увійшов

--- app/home_and_filters.py
+++ app/home_and_filters.py
@@ -1,6 +1,7 @@
 from .loader import app
+from flask import render_template, flash, redirect, url_for
 
 
 @app.route('/')
 def hi():
-    return "Hi"
+    return render_template('base.html')
---
+++ app/product.py
@@ -0,0 +1,2 @@
+from .loader import app
+from flask import render_template, flash, redirect, url_for
