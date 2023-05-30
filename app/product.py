a4fadcc9211abc7ffe2b6d4faee07972ada9168c
vladkolyada <v.kolyada7.13@gmail.com>
2023-05-30 17:32:50 +0300

    трохи попрацював зі задньою сторінкою сайту, в майбутньому треба щоб передавалося ім`я продукту для сторінки

--- app/product.py
+++ app/product.py
@@ -1,2 +1,9 @@
-from .loader import app
+from app.loader import app
 from flask import render_template, flash, redirect, url_for
+from app.data_base import Products
+
+
+@app.route('/product/<product_image_name>')
+def show_product(product_image_name):
+    product = Products.query.filter_by(product_image_name=str(327632806)).first()
+    return render_template('product.html', product=product)
