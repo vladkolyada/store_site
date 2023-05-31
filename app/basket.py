6a551cfa25e958cde837e45568c5f298c7884dd7
vladkolyada <v.kolyada7.13@gmail.com>
2023-05-31 16:26:01 +0300

    ok

--- app/basket.py
+++ app/basket.py
@@ -1,13 +1,28 @@
 from app.loader import app
 from flask import render_template, flash, redirect, url_for
-from .data_base import *
 from app.data_base import Products, Laptops
-
 
 basket = []
 
 
-@app.route('/basket/<c>')
-def basket(c):
+@app.route('/buy/<product_name>')
+def add_to_basket(product_name):
+    global basket
+    basket.append(product_name)
+    return redirect('/product/<product_name>')
+
+
+@app.route('/basket')
+def show_basket():
     global basket
     return render_template('basket.html', basket=basket)
+
+
+@app.route("/delete/<product>")
+def remove_product_from_basket(product):
+    global basket
+    basket.remove(product)
+    return redirect('/basket')
+
+
+
