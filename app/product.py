e6bd4c1e73eddbddcce9e75a46ebb9bcb56d5806
vladkolyada <v.kolyada7.13@gmail.com>
2023-05-31 10:21:44 +0300

    Добаив сам об`єкт бд

--- app/product.py
+++ app/product.py
@@ -1,9 +1,10 @@
 from app.loader import app
 from flask import render_template, flash, redirect, url_for
-from app.data_base import Products
+from app.data_base import Products, Laptops
 
 
 @app.route('/product/<product_image_name>')
 def show_product(product_image_name):
     product = Products.query.filter_by(product_image_name=str(327632806)).first()
-    return render_template('product.html', product=product)
+    product_specs = Laptops.query.filter_by(foreign_key=product.product_id).first()
+    return render_template('product.html', product=product, product_specs=product_specs)
