43b3bea712b1f65bb9d15f1bd68fbd64a88899a3
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-03 00:27:40 +0300

    Налаштував сторінку продукту і вона готова!(Якщо в голову щось ще не прилетить)

--- app/product.py
+++ app/product.py
@@ -5,6 +5,6 @@ from app.data_base import Products, Laptops
 
 @app.route('/product/<product_image_name>')
 def show_product(product_image_name):
-    product = Products.query.filter_by(product_image_name=str(327632806)).first()
+    product = Products.query.filter_by(product_image_name=product_image_name).first()
     product_specs = Laptops.query.filter_by(foreign_key=product.product_id).first()
     return render_template('product.html', product=product, product_specs=product_specs)
