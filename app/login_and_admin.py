fda2fe79c904c378f2f9d6b3c96c6879a2c51122
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-04 12:36:44 +0300

    Вирішив помилку

--- app/login_and_admin.py
+++ app/login_and_admin.py
@@ -2,13 +2,32 @@ from flask import Flask, render_template, redirect, url_for, flash
 
 from . import forms
 from .forms import LogInAdmin, RegistrationForm, LoginUser
-
-from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required,login_manager
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
 from app.data_base import AdminUsers, Users, db
 from app.loader import app
 
+login = LoginManager(app)
+login.login_view = "login"
 
-@app.route('/sign_up',methods=['GET','POST'])
+
+@login.user_loader
+def load_user(id):
+    return Users.query.get(int(id))
+
+
+@login.user_loader
+def load_user(id):
+    return AdminUsers.query.get(int(id))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/sign_up', methods=['GET', 'POST'])
 def signup():
     form = RegistrationForm()
     if form.validate_on_submit():
@@ -22,6 +41,7 @@ def signup():
         login_user(user, remember=form.remember_me.data)
         return redirect('/')
     return render_template('sign_up.html', form=form)
+
 
 @app.route('/log_in', methods=['GET','POST'])
 def log_in():
