1c34a14f9fc33bf37f8afcdba526220c62501b41
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-09 12:15:24 +0300

    багато чого

--- app/home_and_filters.py
+++ app/home_and_filters.py
@@ -1,5 +1,6 @@
 from .loader import app
 from flask import render_template, flash, redirect, url_for
+from .data_base import Products, Laptops
 
 
 @app.route('/')
@@ -7,9 +8,11 @@ def home():
     return render_template('base.html', title='Магазин комп.тех. (HOME)')
 
 
-@app.route('/')
-def filters():
-    pass
+@app.route('/laptops/brand/<filter>')
+def laptop_filter(filter):
+    laptop = Laptops.query.filter_by(brand=filter)
+    product = Products.query.filter_by(id=laptop.foreign_key)
+    render_template('laptops.html', product=product)
 
     
 
