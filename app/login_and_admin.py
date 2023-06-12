126896f8fc08b12fdfa887167401f46833ddab7e
Egor Dombrovskiy <name@gmail.com>
2023-06-12 20:09:37 +0300

    veeerrrry gooooodddddd

--- app/login_and_admin.py
+++ app/login_and_admin.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, url_for, flash, request
 from .forms import LogInAdmin, RegistrationForm, LoginUser, AddProduct
 from flask_login import LoginManager, current_user, login_user, logout_user, login_required
-from app.data_base import AdminUsers, Users, db
+from app.data_base import AdminUsers, Users, db,Products
 from app.loader import app
 
 login = LoginManager(app)
@@ -40,41 +40,45 @@ def signup():
     return render_template('sign_up.html', form=form)
 
 
-@login_required
 @app.route('/add_product', methods=['GET', 'POST'])
 def add_product():
-    form = AddProduct()
-    product_type = request.form.get('product_type')
-    product_image_name = request.form.get('product_image_name')
-    product_title = request.form.get('product_title')
-    product_description = request.form.get('product_description')
-    product_price = request.form.get('product_price')
 
-    if product_type == "laptop":
-        return redirect(
-            url_for('laptop', image_name=product_image_name, title=product_title, description=product_description,
-                    price=product_price))
-    elif product_type == "PC":
-        return redirect(
-            url_for('PC', image_name=product_image_name, title=product_title, description=product_description,
-                    price=product_price))
-    elif product_type == "Phone":
-        return redirect(
-            url_for('Phone', image_name=product_image_name, title=product_title, description=product_description,
-                    price=product_price))
-    elif product_type == "Tablet":
-        return redirect(
-            url_for('Tablet', image_name=product_image_name, title=product_title, description=product_description,
-                    price=product_price))
-    elif product_type == "Mouse":
-        return redirect(
-            url_for('Mouse', image_name=product_image_name, title=product_title, description=product_description,
-                    price=product_price))
-    elif product_type == "Headphones":
-        return redirect(
-            url_for('Headphones', image_name=product_image_name, title=product_title, description=product_description,
-                    price=product_price))
-    return render_template('addproduct.html', form=form)
+    form = AddProduct()
+    if request.method == 'POST':
+        file = request.files['lalala_file']
+        file.save(f'app/static/{file.filename}')
+        print(file)
+        product_type = request.form.get('product_type')
+        product_title = request.form.get('product_title')
+        product_description = request.form.get('product_description')
+        product_price = request.form.get('product_price')
+        base=Products(product_image_name=file.filename,product_type=product_type,product_title=product_title,product_description=product_description,product_price=product_price)
+        db.session.add(base)
+        db.session.commit()
+        if product_type == "Laptop":
+            return redirect(
+                url_for('laptop',image_name=file.filename, title=product_title, description=product_description,
+                        price=product_price))
+        elif product_type == "PC":
+            return redirect(
+                url_for('pc',image_name=file.filename,title=product_title, description=product_description,
+                        price=product_price))
+        elif product_type == "Phone":
+            return redirect(
+                url_for('phone',image_name=file.filename,title=product_title, description=product_description,price=product_price))
+        elif product_type == "Tablet":
+            return redirect(
+                url_for('tablet',image_name=file.filename, title=product_title, description=product_description,
+                        price=product_price))
+        elif product_type == "Mouse":
+            return redirect(
+                url_for('mouse',image_name=file.filename,title=product_title, description=product_description,
+                        price=product_price))
+        elif product_type == "Headphones":
+            return redirect(
+                url_for('head_phones',image_name=file.filename, title=product_title, description=product_description,
+                        price=product_price))
+    return render_template('addproduct.html', form=form,)
 
 
 @app.route('/laptop')
@@ -83,6 +87,7 @@ def laptop():
     title = request.args.get('title')
     description = request.args.get('description')
     price = request.args.get('price')
+
     return render_template('laptop.html', image_name=image_name, title=title, description=description, price=price)
 
 
