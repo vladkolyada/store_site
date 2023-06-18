12cd39adde27c37e38057bf24055186078dced7c
vladkolyada <v.kolyada7.13@gmail.com>
2023-06-18 20:41:48 +0300

    ok..

--- app/basket.py
+++ app/basket.py
@@ -4,11 +4,12 @@ from app.data_base import Products, Orders, db
 
 p_id = 0
 p_type = ''
+total_sum = 0
 
 
 @app.route('/buy/<int:product_id>/<int:quantity>/<product_type>')
 def add_to_basket(product_id, quantity, product_type):
-    global p_id, p_type
+    global p_id, p_type, total_sum
     product = Products.query.filter_by(product_id=product_id).first()
     order = Orders(foreign_key=product.product_id,
                    product_image_name=product.product_image_name,
@@ -19,15 +20,16 @@ def add_to_basket(product_id, quantity, product_type):
     db.session.commit()
     p_id = product_id
     p_type = product_type
+    total_sum += product.product_price*quantity
     return redirect(f'/product_{product_type}/{product_id}')
 
 
 @app.route('/basket')
 def show_basket():
-    global p_id, p_type
+    global p_id, p_type, total_sum
     basket = Orders.query.all()
     p_id = 0
-    return render_template('basket.html', basket=basket, p_type=p_type)
+    return render_template('basket.html', basket=basket, p_type=p_type, total_sum=total_sum)
 
 
 @app.route("/delete/<int:pr_id>")
